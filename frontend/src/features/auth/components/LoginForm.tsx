436a5a96f5d0066ff5b9f4d819ee1de49bea802a
Naoto Abe <naotoabe1@gmail.com>
2026-03-28 21:55:50 +0100

    update form

--- frontend/src/features/auth/components/LoginForm.tsx
+++ frontend/src/features/auth/components/LoginForm.tsx
@@ -5,8 +5,8 @@ export function LoginForm() {
     <form>
       <div className="auth-container">
         <div className="auth-input-container">
-          <label htmlFor='username'>User name</label>
-          <input id="username" name="username" required minLength={2} maxLength={20} autoComplete="username"/>
+          <label htmlFor='email'>User name</label>
+          <input id="email" name="email" required minLength={2} maxLength={20} autoComplete="email"/>
           <label htmlFor='password'>Password</label>
           <input type="password" id="password" name="password" required minLength={8} autoComplete="current-password" />
         </div>
